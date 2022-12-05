ee2d793e0f857b853e545ded0da1be421c983716
TreeNetworkDev <117223242+TreeNetworkDev@users.noreply.github.com>
2022-12-05 10:57:16 -0600

    Set block rewards

--- chia/consensus/block_rewards.py
+++ chia/consensus/block_rewards.py
@@ -5,6 +5,8 @@ from chia.util.ints import uint32, uint64
 # 1 Chia coin = 1,000,000,000,000 = 1 trillion mojo.
 _mojo_per_chia = 1000000000000
 _blocks_per_year = 1681920  # 32 * 6 * 24 * 365
+_tree_per_block = 0.002
+_tree_prefarm = 10000000000000 # 10,000,000,000,000
 
 
 def calculate_pool_reward(height: uint32) -> uint64:
@@ -17,17 +19,17 @@ def calculate_pool_reward(height: uint32) -> uint64:
     """
 
     if height == 0:
-        return uint64(int((7 / 8) * 21000000 * _mojo_per_chia))
-    elif height < 3 * _blocks_per_year:
-        return uint64(int((7 / 8) * 2 * _mojo_per_chia))
-    elif height < 6 * _blocks_per_year:
-        return uint64(int((7 / 8) * 1 * _mojo_per_chia))
-    elif height < 9 * _blocks_per_year:
-        return uint64(int((7 / 8) * 0.5 * _mojo_per_chia))
-    elif height < 12 * _blocks_per_year:
-        return uint64(int((7 / 8) * 0.25 * _mojo_per_chia))
+        return uint64(int((7 / 8) * _tree_prefarm * _mojo_per_chia))
+    # elif height < 3 * _blocks_per_year:
+    #     return uint64(int((7 / 8) * 2 * _mojo_per_chia))
+    # elif height < 6 * _blocks_per_year:
+    #     return uint64(int((7 / 8) * 1 * _mojo_per_chia))
+    # elif height < 9 * _blocks_per_year:
+    #     return uint64(int((7 / 8) * 0.5 * _mojo_per_chia))
+    # elif height < 12 * _blocks_per_year:
+    #     return uint64(int((7 / 8) * 0.25 * _mojo_per_chia))
     else:
-        return uint64(int((7 / 8) * 0.125 * _mojo_per_chia))
+        return uint64(int((7 / 8) * _tree_per_block * _mojo_per_chia))
 
 
 def calculate_base_farmer_reward(height: uint32) -> uint64:
@@ -40,14 +42,14 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
     rates increase continuously.
     """
     if height == 0:
-        return uint64(int((1 / 8) * 21000000 * _mojo_per_chia))
-    elif height < 3 * _blocks_per_year:
-        return uint64(int((1 / 8) * 2 * _mojo_per_chia))
-    elif height < 6 * _blocks_per_year:
-        return uint64(int((1 / 8) * 1 * _mojo_per_chia))
-    elif height < 9 * _blocks_per_year:
-        return uint64(int((1 / 8) * 0.5 * _mojo_per_chia))
-    elif height < 12 * _blocks_per_year:
-        return uint64(int((1 / 8) * 0.25 * _mojo_per_chia))
+        return uint64(int((1 / 8) * _tree_prefarm * _mojo_per_chia))
+    # elif height < 3 * _blocks_per_year:
+    #     return uint64(int((1 / 8) * 2 * _mojo_per_chia))
+    # elif height < 6 * _blocks_per_year:
+    #     return uint64(int((1 / 8) * 1 * _mojo_per_chia))
+    # elif height < 9 * _blocks_per_year:
+    #     return uint64(int((1 / 8) * 0.5 * _mojo_per_chia))
+    # elif height < 12 * _blocks_per_year:
+    #     return uint64(int((1 / 8) * 0.25 * _mojo_per_chia))
     else:
-        return uint64(int((1 / 8) * 0.125 * _mojo_per_chia))
+        return uint64(int((1 / 8) * _tree_per_block * _mojo_per_chia))
